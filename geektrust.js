67abc3e3b33cd443d9d7dafe6ab55d9098f51702
Giriraj Pottekkad <giriraj@geektrust.in>
2022-05-05 17:36:42 +0530

    initial commit

---
+++ geektrust.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+
+fs.readFile(filename, "utf8", (err, data) => {
+    /*if (err) throw err
+    var inputLines = data.toString().split("\n")
+    // Add your code here to process input commands
+    */
+})
---
+++ test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+  
+describe('calculateOverlap', function() {
+    //Add your code here
+});
+
